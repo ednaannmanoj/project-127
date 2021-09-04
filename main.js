79642dfbcef2bad102929bfd41c06abdd423a336
ednaannmanoj <87535624+ednaannmanoj@users.noreply.github.com>
2021-09-04 18:54:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(600,450);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,450);   }
